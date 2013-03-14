e7ef5a8bbd02c04ba4519303374413bb81efe75d
Fisher <fisher@yun.io>
2013-03-14 17:59:52 +0800

    add decoding function

--- quoted_printable.go
+++ quoted_printable.go
@@ -2,9 +2,11 @@ package gob2a
 
 import (
     "bytes"
+    "strings"
+    "errors"
 )
 
-func EncodeQuotedPrintable(bin []byte) string {
+func EncodeQuotedPrintable(bin []byte) (string, error) {
     var (
         l = 0
         buf bytes.Buffer
@@ -20,20 +22,83 @@ func EncodeQuotedPrintable(bin []byte) string {
 
         if b < 32 || b >= 127 || b == '=' || b == '.' {
             buf.WriteByte('=')
-            buf.WriteByte(ToHex(b/16))
-            buf.WriteByte(ToHex(b%16))
+            hex, err := ToHex(b/16)
+            if err != nil {
+                return "", err
+            }
+            buf.WriteByte(hex)
+            hex, err = ToHex(b%16)
+            buf.WriteByte(hex)
             l += 3
         } else {
             buf.WriteByte(b)
             l++
         }
     }
-    return string(buf.Bytes())
+    return string(buf.Bytes()), nil
 }
 
-func ToHex(b byte) byte {
-    if b < 10 {
-        return b + '0'
+func DecodeQuotedPrintable(s string) ([]byte, error) {
+    var (
+        l = 0
+        buf bytes.Buffer
+        a = strings.ToLower(s) //convert to lower first
+    )
+
+    for i := 0; i < len(a); {
+        if l > 74 {
+            return nil, errors.New("invalid encoding source")
+        }
+
+        if a[i] == '=' {
+            if len(a) - i < 2 {
+                return nil, errors.New("too few encoding source")
+            }
+
+            if a[i+1] == 13 && a[i+2] == 10 {
+                l = 0
+                i += 3
+            } else {
+                hh, err := FromHex(a[i+1])
+                if err != nil {
+                    return nil, err
+                }
+
+                lh, err := FromHex(a[i+2])
+                if err != nil {
+                    return nil, err
+                }
+                buf.WriteByte(hh*16 + lh)
+                i += 3
+                l += 3
+            }
+        } else {
+            if err := buf.WriteByte(a[i]); err != nil {
+                return nil, err
+            }
+            l ++
+            i ++
+        }
     }
-    return b - 10 + 'a'
+
+    return buf.Bytes(), nil
+}
+
+func ToHex(b byte) (byte, error) {
+    if b < 10 {
+        return b + '0', nil
+    } else if b < 16 {
+        return b - 10 + 'a', nil
+    }
+
+    return 0, errors.New("invalid number")
+}
+
+func FromHex(h byte) (byte, error) {
+    if h >= 'a' && h <= 'f' {
+        return h - 'a' + 10, nil
+    } else if h >= '0' {
+        return h - '0', nil
+    }
+    return 0, errors.New("invalid hex string")
 }
--- quoted_printable_test.go
+++ quoted_printable_test.go
@@ -4,11 +4,18 @@ import (
     "testing"
 )
 
+const TEST_STRING = "你好"
+
 func TestQuote(t *testing.T){
-    s := EncodeQuotedPrintable([]byte("你好"))
-    if s == "=e4=bd=a0=e5=a5=bd" {
+    s, err := EncodeQuotedPrintable([]byte(TEST_STRING))
+    if err != nil || s != "=e4=bd=a0=e5=a5=bd" {
+        t.Fail()
         return
     }
 
-    t.Fail()
+    b, err := DecodeQuotedPrintable(s)
+    if err != nil || string(b) != TEST_STRING {
+        t.Fail()
+        return
+    }
 }
